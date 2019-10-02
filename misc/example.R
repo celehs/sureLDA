c53f9b481b0b6c90d7efc6a62c438a44c208626b
Ming Yang <hustyangming@gmail.com>
2019-10-02 04:43:51 +0000

    update

--- misc/example.R
+++
@@ -1,9 +0,0 @@
-library(sureLDA)
-
-L <- readRDS("../data/simdata.rds")
-
-set.seed(123)
-surelda_run <- sureLDA(L$X, L$weight, L$ICD, L$NLP, L$HU, L$filter)
-surelda_scores <- surelda_run$scores
-surelda_probs <- surelda_run$probs
-str(surelda_run)
