9509e040c4c387ec5b517d37fbb7a7085f05b86d
Yuri <yuri_ahuja@hms.harvard.edu>
2020-10-22 16:53:09 -0400

    Fixed link error

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Simulated Dataset
 #' 
-#' Click \href{https://github.com/celehs/sureLDA/blob/master/sim/simdata.R}{HERE} to view details.
+#' Click \href{https://github.com/celehs/sureLDA/blob/master/data-raw/simdata.R}{HERE} to view details.
 #' 
 #' @examples 
 #' str(simdata)
